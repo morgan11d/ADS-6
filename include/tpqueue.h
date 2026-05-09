7bd44782eaa1cebfa60fa6e5da8ba0ca2ee370a9
morgan11d <morkicel@gmail.com>
2026-05-09 14:31:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -13,7 +13,7 @@ class TPQueue {
   struct Node {
     T data;
     Node* next;
-    Node(const T& value) : data(value), next(nullptr) {}
+    explicit Node(const T& value) : data(value), next(nullptr) {}
   };
 
   Node* head;
